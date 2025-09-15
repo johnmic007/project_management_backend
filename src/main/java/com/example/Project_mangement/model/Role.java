144aa8dacfdb114b3deeee6b1cc51b95fb2de8ec
john micheal <141893424+johnmic007@users.noreply.github.com>
2025-09-15 14:58:50 +0530

    Initial commit

---
+++ src/main/java/com/example/Project_mangement/model/Role.java
@@ -0,0 +1,18 @@
+package com.yourcompany.projectmanagement.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "roles")
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20, unique = true)
+    private RoleEnum name;
+}
---
+++ src/test/java/com/example/Project_mangement/ProjectMangementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Project_mangement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectMangementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
